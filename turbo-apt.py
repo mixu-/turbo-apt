5dc5563a6eb7760d66ff877a20e674c089b3b59b
Mikko Mattila <mikko.mattila@iki.fi>
2018-05-25 00:11:59 +0300

    EtuoviApt: Restructuring and skeleton for saving/loading

--- turbo-apt.py
+++ turbo-apt.py
@@ -20,24 +20,51 @@ with open("../personal.json", encoding='utf-8') as f:
 class EtuoviApt:
     def __init__(self, url):
         self.url = url
-        with urllib.request.urlopen(url) as response:
-            self.html = response.read()
-        self.soup = BeautifulSoup(self.html, 'html.parser')
-        #Address of interest
-        self.scrapeAddress()
-    
+        if self.load():
+            #The URL was already found from file.
+
+        else:
+            #This is a new URL. Fetch data and save to file.
+            with urllib.request.urlopen(url) as response:
+                self.html = response.read()
+            self.soup = BeautifulSoup(self.html, 'html.parser')
+            #Address of interest
+            self.scrapeAddress()
+            self.commute_times = []
+            for address in _g_addresses:
+                trips = {
+                    "car": getDistance(self.address, address["address"], "driving"), 
+                    "bus": getDistance(self.address, address["address"], "transit")
+                }
+                self.commute_times.append({
+                    "name": address["name"],
+                    "trip_choices": trips})
+            self.save()
+            
     def scrapeAddress(self):
         map_container = self.soup.find('div', id="mapInfoWindowContent")
         self.address = map_container.find_all('label')[0].get_text()
         
     def __str__(self):
         string = self.address + "\n"
-        for address in _g_addresses:
-            car = getDistance(self.address, address["address"], "driving")
-            bus = getDistance(self.address, address["address"], "transit")
-            string += "%s is %s (%s by car, %s by bus)" %(address["name"], car[0], car[1], bus[1]) + "\n"
+        for commute in self.commute_times:
+            trips = commute["trip_choices"]
+            string += "%s is %s (%s by car, %s by bus)" \
+                %(commute["name"], trips["car"][0],
+                  trips["car"][1], trips["bus"][1]) + "\n"
         string += "Commute estimates are based on the next monday at 8.30"
         return string
+
+    def save():
+        """TODO: Saves the object to JSON file."""
+        return
+
+    def load():
+        """TODO: Loads an object from JSON file.
+        
+        Returns:
+            True for existing EtuoviApt objects. Else False."""
+        return False
        
 def getDistance(origin, destination, mode="driving"):
     time_of_travel = int(next_weekday(datetime.datetime.now(), 0, 8, 30).timestamp())
@@ -50,7 +77,7 @@ def getDistance(origin, destination, mode="driving"):
            }
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?" + urlencode(data)
     
-    print(url)
+    #print(url)
     try:
         result = simplejson.load(urllib.request.urlopen(url))
     except Exception as e:
