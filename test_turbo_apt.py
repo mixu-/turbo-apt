1632c610ed526d102418e8c6236426cad8474e58
Mikko Mattila <mikko.mattila@iki.fi>
2018-05-26 19:54:41 +0300

    Added EtuoviApt save(), load() and test.

--- test_turbo_apt.py
+++ test_turbo_apt.py
@@ -1,19 +1,50 @@
 import unittest
 import pprint
+import os
+import json
 import turbo_apt
 
-
+_g_jsonfile = "test.json"
+_g_test_urls = ["https://www.etuovi.com/kohde/620468", "https://www.etuovi.com/kohde/9988654"]
 class TurboTestCase(unittest.TestCase):
     def setUp(self):
         pass
     def tearDown(self):
         pass
     def testAddAptAndPrint(self):
-        apt = turbo_apt.EtuoviApt("https://www.etuovi.com/kohde/9449326")
+        apt = turbo_apt.EtuoviApt(_g_test_urls[0])
         print(apt)
 
-    def testGetDistance(self):
-        turbo_apt.getDistance("Seattle", "Los Angeles", "transit")
-        car = turbo_apt.getDistance("Seattle", "Los Angeles")
-        pprint.pprint(car)
-        assert int(car[2]) > 1700000 and int(car[2]) < 1900000
+    #def testGetDistance(self):
+    #    turbo_apt.getDistance("Seattle", "Los Angeles", "transit")
+    #    car = turbo_apt.getDistance("Seattle", "Los Angeles")
+    #    pprint.pprint(car)
+    #    assert int(car[2]) > 1700000 and int(car[2]) < 1900000
+
+    def testSaveAndLoad(self):
+        def checkFile(apt):
+            assert os.path.exists(_g_jsonfile), "File not found! {}".format(_g_jsonfile)
+            with open(_g_jsonfile, "r", encoding="utf-8") as fp:
+                file_list = json.load(fp)
+                pprint.pprint(file_list)
+            
+            found = True
+            for obj in file_list:
+                if obj["url"] == apt.url:
+                    found = True
+                    assert apt.address == obj["address"], \
+                        "Address incorrect! Got {}, expected {}".format(apt.address, obj["address"])
+            assert found, "Apartment missing from json file! ({})\n{}".format(apt.address, file_list)
+        if os.path.exists(_g_jsonfile):
+            os.unlink(_g_jsonfile)
+        apt0 = turbo_apt.EtuoviApt(_g_test_urls[0], json_file=_g_jsonfile)
+        checkFile(apt0)
+        apt1 = turbo_apt.EtuoviApt(_g_test_urls[1], json_file=_g_jsonfile)
+        checkFile(apt1)
+        #pprint.pprint(file_dict)
+
+        #Now, load the same url as with apt0. The update time should be same if loading works.
+        apt2 = turbo_apt.EtuoviApt(_g_test_urls[0], _g_jsonfile)
+        assert apt2.last_updated == apt0.last_updated, \
+            "Apt load() failed (timestamp has changed) " \
+                + str(apt2.last_updated) + " != " + str(apt0.last_updated)
--- turbo_apt.py
+++ turbo_apt.py
@@ -1,8 +1,12 @@
+import os
 import urllib
 import urllib.parse
 from urllib.parse import urlencode, quote_plus
+import time
 import datetime
 import json
+import re
+from json import JSONEncoder, JSONDecoder
 from bs4 import BeautifulSoup
 import simplejson
 
@@ -10,40 +14,49 @@ _g_addresses = []
 
 _g_api_key = None
 
+def log(msg, level="info"):
+    print(msg)
+
 with open("../personal.json", encoding='utf-8') as f:
    data = json.load(f)
    _g_addresses = data["addresses"]
-   print(_g_addresses)
+   #log(_g_addresses)
    _g_api_key = data["google_api_key"]
 
 
 class EtuoviApt:
-    def __init__(self, url):
+    def __init__(self, url, json_file="turbo_apt.json"):
         self.url = url
-        if self.load():
+        self.commute_times = []
+        self._html = ""
+        self._soup = None
+        self.last_updated = 0
+        self.address = ""
+        if self.load(json_file):
             #The URL was already found from file.
-            pass
+            log("Loaded %s" %(self.address))
         else:
             #This is a new URL. Fetch data and save to file.
             with urllib.request.urlopen(url) as response:
-                self.html = response.read()
-            self.soup = BeautifulSoup(self.html, 'html.parser')
+                self._html = response.read()
+            self._soup = BeautifulSoup(self._html, 'html.parser')
             #Address of interest
             self.scrapeAddress()
-            self.commute_times = []
+            self.last_updated = time.time()
             for address in _g_addresses:
                 trips = {
                     "car": getDistance(self.address, address["address"], "driving"), 
                     "bus": getDistance(self.address, address["address"], "transit")
-                }
+               }
                 self.commute_times.append({
                     "name": address["name"],
                     "trip_choices": trips})
-            self.save()
+            self.save(json_file)
             
     def scrapeAddress(self):
-        map_container = self.soup.find('div', id="mapInfoWindowContent")
+        map_container = self._soup.find('div', id="mapInfoWindowContent")
         self.address = map_container.find_all('label')[0].get_text()
+        log("Address:" + self.address)
         
     def __str__(self):
         string = self.address + "\n"
@@ -55,17 +68,52 @@ class EtuoviApt:
         string += "Commute estimates are based on the next monday at 8.30"
         return string
 
-    def save(self):
-        """TODO: Saves the object to JSON file."""
-        return
+    def save(self, filepath):
+        """Saves the object to JSON file."""
+        
+        target = []
+        if os.path.exists(filepath):
+            with open(filepath, "r") as fp:
+                target = json.load(fp)
+            for obj in target:
+                if type(obj) != dict:
+                    raise Exception("Invalid JSON! List contained %s, expected only dict" %(type(obj), ))
+                if obj["address"] == self.address:
+                    #Remove the existing entry, we'll add it back later.
+                    target.remove(obj)
+                    log("Removed {}".format(obj["address"]))
+            
+        tmp_dict = self.__dict__
+        #Don't save private keys to file. soup is not serializeable.
+        for key in tmp_dict.keys():
+            if key.startswith("_"):
+                tmp_dict[key] = None
+        
+        target.append(tmp_dict)
 
-    def load(self):
-        """TODO: Loads an object from JSON file.
+        with open(filepath, "w") as fp:
+            fp.write(json.dumps(target, indent=4, sort_keys=True))
+
+    def load(self, filepath):
+        """Loads an object from JSON file.
         
         Returns:
             True for existing EtuoviApt objects. Else False."""
+
+        if not os.path.exists(filepath):
+            return False
+        with open(filepath, "r", encoding="utf-8") as fp:
+            json_list = json.load(fp)
+        for obj in json_list:
+            if "address" in obj and obj["url"] == self.url:
+                log("Found match in file for %s" %(self.url))
+                for prop in self.__dict__.keys():
+                    if not prop.startswith("_"):
+                        setattr(self, prop, obj[prop])
+                return True
         return False
-       
+
+
 def getDistance(origin, destination, mode="driving"):
     time_of_travel = int(next_weekday(datetime.datetime.now(), 0, 8, 30).timestamp())
     data = {"origins": origin,
